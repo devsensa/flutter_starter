36fff4cfcafb7d57c1f605e6bd12b64a829d03a9
devsensa <person.sensa@gmail.com>
2020-04-11 01:56:52 +0300

    Update init.sh
    - remove `--no-overwrite` key

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 PROJECT_NAME=$(basename $(pwd))
-flutter create --no-overwrite -i swift -a kotlin --org me.sensa --project-name $PROJECT_NAME .
+flutter create -i swift -a kotlin --org me.sensa --project-name $PROJECT_NAME .
